b7108a1801ee42ed2fd7264f57752a49661d9003
Rémi Dupré <remim.dupre@gmail.com>
2019-07-01 13:05:26 +0200

    travis_norify: update message for pre-commit

--- scripts/travis_notify_unformatted.sh
+++ scripts/travis_notify_unformatted.sh
@@ -1,8 +1,13 @@
 COMMENT=$(sed ':a;N;$!ba;s/\n/\\\\n/g' <<EOF
 Your branch contains non-formatted code.
 
-Please reformat your code by running `black .` at the root of the project. You
-may consider automating this process by installing [this git hook](https://gist.github.com/mrasband/fa8c07a1c63d685f4796c439089ef3ab).
+Please reformat your code by running `black .` at the root of the project.
+You may consider automating this process by installing default git hooks:
+
+```bash
+pip install pre-commit
+pre-commit install
+```
 EOF
 )
 
